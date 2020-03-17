8f6afc89c46a7546287445d6c258c85d1ba72dc5
LiubovKleimenova <kleimenovaliubov@gmail.com>
2020-03-17 15:21:24 -0400

    rendered day in app.js

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,6 +1,27 @@
 import React from "react";
 
 import "components/Application.scss";
+import Button from "components/Button";
+import DayListItem from "components/DayListItem";
+import DayList from "components/DayList";
+
+const days = [
+  {
+    id: 1,
+    name: "Monday",
+    spots: 2
+  },
+  {
+    id: 2,
+    name: "Tuesday",
+    spots: 5
+  },
+  {
+    id: 3,
+    name: "Wednesday",
+    spots: 0
+  }
+]
 
 export default function Application(props) {
   return (
@@ -12,7 +33,13 @@ export default function Application(props) {
           alt="Interview Scheduler"
         />
         <hr className="sidebar__separator sidebar--centered" />
-        <nav className="sidebar__menu"></nav>
+        <nav className="sidebar__menu">
+          <DayList
+            days={days}
+            day={"Monday"}
+            setDay={day => console.log(day)}
+          />
+        </nav>
         <img
           className="sidebar__lhl sidebar--centered"
           src="images/lhl.png"
