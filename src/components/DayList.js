d16adec71c2f2dc38f3b5f5fe6c7268029cbdf9c
LiubovKleimenova <kleimenovaliubov@gmail.com>
2020-03-17 09:08:19 -0400

    created DayList

---
+++ src/components/DayList.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "components/DayListItem.scss";
+var classNames = require("classnames");
+
+export default function DayList(props) {
+  return (
+  <ul></ul>
+  )
+}
