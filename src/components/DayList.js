8e6deb57a02c1e26e7147f494332ea2b301db628
LiubovKleimenova <kleimenovaliubov@gmail.com>
2020-03-17 15:10:13 -0400

    rendered DayList for each day

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,9 +1,18 @@
 import React from "react";
-import "components/DayListItem.scss";
-var classNames = require("classnames");
+import DayListItem from "components/DayListItem";
 
 export default function DayList(props) {
-  return (
-  <ul></ul>
-  )
+  return (<ul>
+    {props.days.map(day => (
+      <li>
+        <DayListItem
+          key={day.id}
+          name={day.name}
+          spots={day.spots}
+          selected={day.name === props.day}
+          setDay={props.setDay}
+        />
+      </li>
+    ))}
+  </ul>)  
 }
--- stories/index.js
+++ stories/index.js
@@ -7,7 +7,7 @@ import "index.scss";
 
 import Button from "components/Button";
 import DayListItem from "components/DayListItem";
-import DayList from "components/DayListItem";
+import DayList from "components/DayList";
 
 storiesOf("Button", module)
   .addParameters({
@@ -36,3 +36,32 @@ storiesOf("DayListItem", module)
   .add("Clickable", () => (
     <DayListItem name="Tuesday" setDay={action("setDay")} spots={5} />
   )); 
+
+const days = [
+  {
+    id: 1,
+    name: "Monday",
+    spots: 2
+  },
+  {
+    id: 2,
+    name: "Tuesday",
+    spots: 5
+  },
+  {
+    id: 3,
+    name: "Wednesday",
+    spots: 0
+  }
+];
+
+storiesOf("DayList", module)
+  .addParameters({
+    backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+  })
+  .add("Monday", () => (
+    <DayList days={days} day={"Monday"} setDay={action("setDay")} />
+  ))
+  .add("Tuesday", () => (
+    <DayList days={days} day={"Tuesday"} setDay={action("setDay")} />
+  ));
