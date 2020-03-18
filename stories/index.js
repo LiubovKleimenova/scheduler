3eef502f0282e359a0a0f7b36e110c19879f5af5
LiubovKleimenova <kleimenovaliubov@gmail.com>
2020-03-18 08:41:18 -0400

    created Form stories

--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 storiesOf("Button", module)
   .addParameters({
@@ -158,12 +159,26 @@ storiesOf("InterviewerList", module)
         onCancel={action("onCancel")}
       />
     ))
-    .add("Status", () =>
-      (<Status message="Deleting" />))
+    .add("Status", () => <Status message="Deleting" />)
     .add("Error", () => (
       <Error
         message="Could not delete appointment."
         onClose={action("onClose")}
       />
     ))
-    ;
+    .add("Edit Form", () => (
+      <Form
+        name="Archie Cohen"
+        interviewers={interviewers}
+        interviewer={1}
+        onSave={action("onSave")}
+        onCancel={action("onCancel")}
+      />
+    ))
+    .add("Create Form", () => (
+      <Form
+        interviewers={interviewers}
+        onSave={action("onSave")}
+        onCancel={action("onCancel")}
+      />
+    ));
