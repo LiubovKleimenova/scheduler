390c214dc928a6d3fefa97e507daa5c681e94920
LiubovKleimenova <kleimenovaliubov@gmail.com>
2020-03-18 15:15:13 -0400

    added appoinmtm compt

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,9 +1,15 @@
 import React from "react";
 import "components/Appointment/styles.scss";
 //let classNames = require("classnames");
+import Header from "components/Appointment/Header";
+import Show from "components/Appointment/Show";
+import Empty from "components/Appointment/Empty";
 
 export default function Appointment(props) {
   return (
-    <article className="appointment"></article>
-  )
+    <article className="appointment">
+      <Header time={props.time} />
+      {props.interview ? <Show /> : <Empty />}
+    </article>
+  );
 }
--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -182,4 +182,10 @@ storiesOf("InterviewerList", module)
         onSave={action("onSave")}
         onCancel={action("onCancel")}
       />
+    ))
+    .add("Appointment Empty", () => (
+      <Fragment>
+        <Appointment id={1} time="12pm" />
+        <Appointment id="last" time="1pm" />
+      </Fragment>
     ));
