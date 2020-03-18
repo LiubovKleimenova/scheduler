c6512143b961e0405c42677eeb542c21ba287ca9
LiubovKleimenova <kleimenovaliubov@gmail.com>
2020-03-18 00:00:42 -0400

    created Status component

---
+++ src/components/Appointment/Status.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Confirm(props) {
+  return (
+    <main className="appointment__card appointment__card--status">
+      <img
+        className="appointment__status-image"
+        src="images/status.png"
+        alt="Loading"
+      />
+      <h1 className="text--semi-bold">Deleting</h1>
+    </main>
+  );
+}
--- stories/index.js
+++ stories/index.js
@@ -15,6 +15,7 @@ import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
 
 storiesOf("Button", module)
   .addParameters({
