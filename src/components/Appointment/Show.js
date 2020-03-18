85b8bc2d71d67fd5c86e3918ffe8fd7f328f6141
LiubovKleimenova <kleimenovaliubov@gmail.com>
2020-03-17 23:00:13 -0400

    added props to Show comp-t

--- src/components/Appointment/Show.js
+++ src/components/Appointment/Show.js
@@ -4,10 +4,10 @@ export default function Show(props) {
   return (
     <main className="appointment__card appointment__card--show">
       <section className="appointment__card-left">
-        <h2 className="text--regular">Lydia Miller-Jones</h2>
+        <h2 className="text--regular">{props.student}</h2>
         <section className="interviewer">
           <h4 className="text--light">Interviewer</h4>
-          <h3 className="text--regular">Sylvia Palmer</h3>
+          <h3 className="text--regular">{props.interviewer.name}</h3>
         </section>
       </section>
       <section className="appointment__card-right">
@@ -16,11 +16,13 @@ export default function Show(props) {
             className="appointment__actions-button"
             src="images/edit.png"
             alt="Edit"
+            onClick={props.onEdit}
           />
           <img
             className="appointment__actions-button"
             src="images/trash.png"
             alt="Delete"
+            onClick={props.onDelete}
           />
         </section>
       </section>
