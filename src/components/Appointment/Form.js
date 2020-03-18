660980e521c7dd218e1e96950542cda22a8669be
LiubovKleimenova <kleimenovaliubov@gmail.com>
2020-03-18 08:31:42 -0400

    created Form compt

---
+++ src/components/Appointment/Form.js
@@ -0,0 +1,35 @@
+import React, {useState} from "react";
+
+import InterviewerList from "components/InterviewerList";
+import Button from "components/Button";
+
+export default function Form(props) {
+  return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form autoComplete="off">
+          <input
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            type="text"
+            placeholder="Enter Student Name"
+            /*
+          This must be a controlled component
+        */
+          />
+        </form>
+        <InterviewerList
+          interviewers={props.interviewers}
+          value={interviewer}
+          onChange={setInterviewer}
+        />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger>Cancel</Button>
+          <Button confirm>Save</Button>
+        </section>
+      </section>
+    </main>
+  );
+}
