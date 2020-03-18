10d237153a5f937b859e3811c7fa72fee8afb358
LiubovKleimenova <kleimenovaliubov@gmail.com>
2020-03-17 22:50:50 -0400

    added event handler on Empty

--- src/components/Appointment/Empty.js
+++ src/components/Appointment/Empty.js
@@ -3,10 +3,11 @@ import React from "react";
 export default function Empty(props) {
   return (
     <main className="appointment__add">
-      <img 
-        className="appointment__add-button" 
-        src="images/add.png" 
-        alt="Add" 
+      <img
+        className="appointment__add-button"
+        src="images/add.png"
+        alt="Add"
+        onClick={props.onAdd}
       />
     </main>
   );
