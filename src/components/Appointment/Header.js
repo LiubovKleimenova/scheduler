e87ebf6f94a15cb6497a62db89b4db593ce6ca56
LiubovKleimenova <kleimenovaliubov@gmail.com>
2020-03-17 22:19:36 -0400

    created header component

---
+++ src/components/Appointment/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+//import "components/Appointment/styles.scss";
+//let classNames = require("classnames");
+
+export default function Header(props) {
+  return (
+    <header className="appointment__time">
+      <h4 className="text--semi-bold">12pm</h4>
+      <hr className="appointment__separator" />
+    </header>
+  );
+}
