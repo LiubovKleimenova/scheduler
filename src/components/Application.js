301d73cf9f1681cd826ba190b454435b2e26bf44
LiubovKleimenova <kleimenovaliubov@gmail.com>
2020-03-18 15:49:14 -0400

    imported appts to applctn

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,6 +2,7 @@ import React, {useState} from "react";
  
 import "components/Application.scss";
 import DayList from "components/DayList";
+import Appointment from "components/Appointment";
 
 const days = [
   {
