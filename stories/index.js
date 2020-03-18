745b8e5e990ad2977d4e2230175637a7b7cea164
LiubovKleimenova <kleimenovaliubov@gmail.com>
2020-03-18 00:11:52 -0400

    fixed error stories

--- stories/index.js
+++ stories/index.js
@@ -16,6 +16,7 @@ import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error";
 
 storiesOf("Button", module)
   .addParameters({
@@ -157,8 +158,12 @@ storiesOf("InterviewerList", module)
         onCancel={action("onCancel")}
       />
     ))
-    .add("Status", () => (
-      <Status
-        message="Deleting"
+    .add("Status", () =>
+      (<Status message="Deleting" />))
+    .add("Error", () => (
+      <Error
+        message="Could not delete appointment."
+        onClose={action("onClose")}
       />
-    ));
+    ))
+    ;
