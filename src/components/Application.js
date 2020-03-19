95fe4edb84e2eae05db1b489c0990a2ff09e01cd
LiubovKleimenova <kleimenovaliubov@gmail.com>
2020-03-18 22:50:06 -0400

    set state for days, day, appt

--- src/components/Application.js
+++ src/components/Application.js
@@ -49,19 +49,25 @@ const appointments = [
 ];
 
 export default function Application(props) {
-  const [days, setDays] = useState([]);
-  const [day, setDay] = useState("Monday")
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
+
+  const setDay = day => setState({ ...state, day });
+  const setDays = days => setState(prev => ({ ...prev, days }));
 
   useEffect(()=>{
     axios.get(`/api/days`)
-    .then(response => {
-      setDays(() => response.data);
-    })
-    .catch(err => {
+    .then(response => 
+      setDays(response.data)
+    )
+    .catch(err => 
       console.error(err)
-    })
-  }, [days])
-  
+    )
+  }, [])
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -72,7 +78,7 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList days={days} day={day} setDay={setDay} />
+          <DayList days={state.days} day={state.day} setDay={setDay} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
