ae0ebf17785d9bb1bfa39120dbcf92315dfc4c0f
LiubovKleimenova <kleimenovaliubov@gmail.com>
2020-03-19 18:16:09 -0400

    applied hook to appointment

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,6 +4,10 @@ import "components/Appointment/styles.scss";
 import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
+import useVisualMode from "hooks/useVisualMode";
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
 
 export default function Appointment(props) {
   return (
