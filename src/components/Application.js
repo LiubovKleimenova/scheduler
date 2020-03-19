0e3a4974d54f9f71747c0bb9a9da665f4f556ca1
LiubovKleimenova <kleimenovaliubov@gmail.com>
2020-03-19 11:43:41 -0400

    added interviewers to state object

--- src/components/Application.js
+++ src/components/Application.js
@@ -10,7 +10,8 @@ export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
 
   const setDay = day => setState({ ...state, day });
