7e374267f387afa3f16df70f8286bb484f8d9073
LiubovKleimenova <kleimenovaliubov@gmail.com>
2020-03-19 17:32:00 -0400

    created custom Hook to control mode

--- src/hooks/__tests__/useVisualMode.test.js
+++ src/hooks/__tests__/useVisualMode.test.js
@@ -1,3 +1,43 @@
 import { renderHook, act } from "@testing-library/react-hooks";
 
 import useVisualMode from "hooks/useVisualMode";
+
+const FIRST = "FIRST";
+const SECOND = "SECOND";
+const THIRD = "THIRD";
+
+test("useVisualMode should initialize with default value", () => {
+  const { result } = renderHook(() => useVisualMode(FIRST));
+
+  expect(result.current.mode).toBe(FIRST);
+});
+
+test("useVisualMode should transition to another mode", () => {
+  const { result } = renderHook(() => useVisualMode(FIRST));
+
+  act(() => result.current.transition(SECOND));
+  expect(result.current.mode).toBe(SECOND);
+});
+
+test("useVisualMode should return to previous mode", () => {
+  const { result } = renderHook(() => useVisualMode(FIRST));
+
+  act(() => result.current.transition(SECOND));
+  expect(result.current.mode).toBe(SECOND);
+
+  act(() => result.current.transition(THIRD));
+  expect(result.current.mode).toBe(THIRD);
+
+  act(() => result.current.back());
+  expect(result.current.mode).toBe(SECOND);
+
+  act(() => result.current.back());
+  expect(result.current.mode).toBe(FIRST);
+});
+
+test("useVisualMode should not return to previous mode if already at initial", () => {
+  const { result } = renderHook(() => useVisualMode(FIRST));
+
+  act(() => result.current.back());
+  expect(result.current.mode).toBe(FIRST);
+});
---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,18 @@
+import { useState } from "react";
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+  function transition(newMode, replace = false) {
+    if (!replace) {
+      setHistory(() => [...history, newMode]);
+    }
+    setMode(() => newMode);
+  }
+  function back() {
+    if (history[history.length - 1] !== initial) {
+      history.pop();
+      setMode(() => history[history.length - 1]);
+    }
+  }
+  return { mode, transition, back };
+}
