ce48117fcf3a93bca0925f2e43baad61a63603a3
LiubovKleimenova <kleimenovaliubov@gmail.com>
2020-03-18 21:30:13 -0400

    added getAppts4Day fn

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,1 +1,14 @@
-export function getAppointmentsForDay(state, day) {}
+export function getAppointmentsForDay(state, day) {
+  let filteredDays = state.days.filter(newDay => day === newDay.name);
+  if (!(filteredDays !== [] && day && filteredDays[0])) {
+    return [];
+  }
+  const appointments = filteredDays[0].appointments;
+  const result = [];
+  for (let key in state.appointments) {
+    if (appointments.includes(state.appointments[key].id)) {
+      result.push(state.appointments[key]);
+    }
+  }
+  return result;
+}
