3b29379fabe1024ed798f5d2685f041e537c88a9
LiubovKleimenova <kleimenovaliubov@gmail.com>
2020-03-19 18:36:24 -0400

    rconditionally rendered appt show/empty

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -10,16 +10,18 @@ const EMPTY = "EMPTY";
 const SHOW = "SHOW";
 
 export default function Appointment(props) {
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {props.interview ? (
-        (<Show
+      {mode === EMPTY && <Empty onAdd={props.onAdd} />}
+      {mode === SHOW && (
+        <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
-        />)
-      ) : (
-        <Empty />
+        />
       )}
     </article>
   );
