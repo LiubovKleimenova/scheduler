99ce33bdfe3da5a42b4f105c2f247d2c3fe483bb
LiubovKleimenova <kleimenovaliubov@gmail.com>
2020-03-20 13:10:20 -0400

    added CREATE mode + transition/back on click

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,15 +4,20 @@ import "components/Appointment/styles.scss";
 import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
+import Form from "components/Appointment/Form";
 import useVisualMode from "hooks/useVisualMode";
 
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 export default function Appointment(props) {
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
+
+  //props.interviewers = [];
+
   return (
     <article className="appointment">
       <Header time={props.time} />
@@ -21,6 +26,12 @@ export default function Appointment(props) {
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
+        />
+      )}
+      {mode === CREATE && (
+        <Form
+          interviewers={[]}
+          onCancel={() => back()}
         />
       )}
     </article>
