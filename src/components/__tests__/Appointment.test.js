7b23dbdb40a675169d570d458d882f60014a391e
LiubovKleimenova <kleimenovaliubov@gmail.com>
2020-03-23 19:08:06 -0400

    added form tests

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -6,6 +6,8 @@ import Appointment from "components/Appointment";
 
 afterEach(cleanup);
 
-it("renders without crashing", () => {
-  render(<Appointment />);
+describe("Appointment", () => {
+  it("renders without crashing", () => {
+    render(<Appointment />);
+  });
 });
---
+++ src/components/__tests__/Form.test.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+import { render, cleanup } from "@testing-library/react";
+
+import Form from "components/Appointment/Form";
+
+afterEach(cleanup);
+
+describe("Form", () => {
+  const interviewers = [
+    {
+      id: 1,
+      name: "Sylvia Palmer",
+      avatar: "https://i.imgur.com/LpaY82x.png"
+    }
+  ];
+
+  it("renders without student name if not provided", () => {
+    expect(getByPlaceholderText("Enter Student Name")).toHaveValue("");
+  });
+
+  it("renders with initial student name", () => {
+    expect(getByTestId("student-name-input")).toHaveValue("Lydia Miller-Jones");
+  });
+});
