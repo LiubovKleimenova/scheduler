e3afc1dee38458a7d398cc2289161fa658e77faa
LiubovKleimenova <kleimenovaliubov@gmail.com>
2020-03-23 12:11:11 -0400

    importeed prop-types

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "components/InterviewerList.scss";
 import InterviewerListItem from "components/InterviewerListItem";
+import PropTypes from "prop-types";
 
 export default function InterviewerList(props) {
   return (
