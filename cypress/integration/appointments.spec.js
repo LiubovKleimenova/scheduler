8bc4b2f54819824cc3d6d0015c007c083d8d0080
LiubovKleimenova <kleimenovaliubov@gmail.com>
2020-03-25 18:26:33 -0400

    added cy test to delete interview

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -37,7 +37,12 @@ describe("Appointments", () => {
   });
 
   it("should cancel an interview", () => {
-    
-    
+    cy.get("[alt=Delete]")
+      .first()
+      .click({ force: true });
+    cy.contains("Confirm").click();
+    cy.contains("Deleting").should("exist");
+    cy.contains("Deleting").should("not.exist");
+    cy.contains(".appointment__card--show", "Archie Cohen").should("not.exist");
   });
 });
