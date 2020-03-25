8342757af1acf27d71906eae14f8b13029e30279
LiubovKleimenova <kleimenovaliubov@gmail.com>
2020-03-25 17:12:44 -0400

    refactored test with data-id

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -5,5 +5,9 @@ describe("Navigation", () => {
 
   it("should navigate to Tuesday", () => {
     cy.visit("/");
+    cy.contains("[data-testid=day]", "Tuesday")
+      .click()
+      .should("have.css", "background-color", "rgb(242, 242, 242)");
+    
   });
 });
